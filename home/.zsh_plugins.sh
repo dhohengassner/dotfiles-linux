528ed1b62dfb45d09454ceffbc1b0621757a629b
David Hohengaßner <dhohengassner@bethel.jw.org>
2020-07-29 12:49:13 +0200

    latest alcritty and plugin updates

--- home/.zsh_plugins.sh
+++ home/.zsh_plugins.sh
@@ -1,20 +1,20 @@
-source /home/dh/snap/code/common/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-robbyrussell-SLASH-oh-my-zsh/oh-my-zsh.sh
-fpath+=( /home/dh/snap/code/common/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-robbyrussell-SLASH-oh-my-zsh )
-source /home/dh/snap/code/common/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-robbyrussell-SLASH-oh-my-zsh/plugins/aws/aws.plugin.zsh
-fpath+=( /home/dh/snap/code/common/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-robbyrussell-SLASH-oh-my-zsh/plugins/aws )
-source /home/dh/snap/code/common/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-robbyrussell-SLASH-oh-my-zsh/plugins/kubectl/kubectl.plugin.zsh
-fpath+=( /home/dh/snap/code/common/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-robbyrussell-SLASH-oh-my-zsh/plugins/kubectl )
-source /home/dh/snap/code/common/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-popstas-SLASH-zsh-command-time/command-time.plugin.zsh
-fpath+=( /home/dh/snap/code/common/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-popstas-SLASH-zsh-command-time )
-source /home/dh/snap/code/common/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-autosuggestions/zsh-autosuggestions.plugin.zsh
-fpath+=( /home/dh/snap/code/common/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-autosuggestions )
-source /home/dh/snap/code/common/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-syntax-highlighting/zsh-syntax-highlighting.plugin.zsh
-fpath+=( /home/dh/snap/code/common/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-syntax-highlighting )
-source /home/dh/snap/code/common/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-docker-SLASH-cli/contrib/completion/zsh/_docker
-source /home/dh/snap/code/common/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-djui-SLASH-alias-tips/alias-tips.plugin.zsh
-fpath+=( /home/dh/snap/code/common/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-djui-SLASH-alias-tips )
-source /home/dh/snap/code/common/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-rupa-SLASH-z/z.sh
-fpath+=( /home/dh/snap/code/common/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-rupa-SLASH-z )
-source /home/dh/snap/code/common/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-andsens-SLASH-homeshick/homeshick.sh
-fpath+=( /home/dh/snap/code/common/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-andsens-SLASH-homeshick )
-fpath+=( /home/dh/snap/code/common/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-andsens-SLASH-homeshick/completions )
+source /home/dh/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-robbyrussell-SLASH-oh-my-zsh/oh-my-zsh.sh
+fpath+=( /home/dh/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-robbyrussell-SLASH-oh-my-zsh )
+source /home/dh/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-robbyrussell-SLASH-oh-my-zsh/plugins/aws/aws.plugin.zsh
+fpath+=( /home/dh/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-robbyrussell-SLASH-oh-my-zsh/plugins/aws )
+source /home/dh/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-robbyrussell-SLASH-oh-my-zsh/plugins/kubectl/kubectl.plugin.zsh
+fpath+=( /home/dh/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-robbyrussell-SLASH-oh-my-zsh/plugins/kubectl )
+source /home/dh/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-popstas-SLASH-zsh-command-time/command-time.plugin.zsh
+fpath+=( /home/dh/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-popstas-SLASH-zsh-command-time )
+source /home/dh/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-autosuggestions/zsh-autosuggestions.plugin.zsh
+fpath+=( /home/dh/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-autosuggestions )
+source /home/dh/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-syntax-highlighting/zsh-syntax-highlighting.plugin.zsh
+fpath+=( /home/dh/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-syntax-highlighting )
+source /home/dh/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-docker-SLASH-cli/contrib/completion/zsh/_docker
+source /home/dh/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-djui-SLASH-alias-tips/alias-tips.plugin.zsh
+fpath+=( /home/dh/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-djui-SLASH-alias-tips )
+source /home/dh/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-rupa-SLASH-z/z.sh
+fpath+=( /home/dh/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-rupa-SLASH-z )
+source /home/dh/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-andsens-SLASH-homeshick/homeshick.sh
+fpath+=( /home/dh/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-andsens-SLASH-homeshick )
+fpath+=( /home/dh/.cache/antibody/https-COLON--SLASH--SLASH-github.com-SLASH-andsens-SLASH-homeshick/completions )
